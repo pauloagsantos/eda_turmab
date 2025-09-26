b681938134f4945a81eda5dc0f016332cb71e017
Paulo Santos <pauloagsantos@gmail.com>
2025-09-26 10:02:02 +0100

    Update Ficha1.java

--- Ficha1/src/ficha1/Ficha1.java
+++ Ficha1/src/ficha1/Ficha1.java
@@ -9,15 +9,24 @@ package ficha1;
  * @author IPT
  */
 public class Ficha1 {
+    
+    public static long runXtimes(int value, int x) {
+        int sum=0;
+        for(int i = 0; i < x; i++) {
+             long ini = System.nanoTime();
+             Matematica.factorialI(value);
+             long fim = System.nanoTime();
+             sum += fim-ini;
+        }
+        return sum/x;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for(int i = 0; i < 20; i++) {
-            System.out.println(i+"!="+Matematica.factorialR(i));
-        }
+        System.out.println(runXtimes(1000, 10));
     }
     
 }
