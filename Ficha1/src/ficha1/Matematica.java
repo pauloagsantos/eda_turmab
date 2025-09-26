23c4da2006b1ec93e1c70955453566a198016f6e
Paulo Santos <pauloagsantos@gmail.com>
2025-09-26 09:42:34 +0100

    Update Matematica.java

--- Ficha1/src/ficha1/Matematica.java
+++ Ficha1/src/ficha1/Matematica.java
@@ -10,18 +10,26 @@ package ficha1;
  */
 public class Matematica {
     
+    // 5N+4
+    // O(N) Complexidade temporal linear
+    // num+f+i+return (4 variáveis)  
+    // S(1) Complexidade espacial constante
     public static int factorialI(int num){
-        int f = 1;
-        for(int i = 1; i <= num; i++)
-            f*=i;
-        return f;
+        int f = 1;                     // 1
+        for(int i = 1; i <= num; i++)  // 1 + N + 1 + 2N
+            f*=i;                      // 2N
+        return f;                      // 1
     }
     
+    // 5N + 2
+    // O(N) Complexidade temporal linear
+    // (num+return) * (N+1 da Recursividade) = 2N+2
+    // S(N) Complexidade espacial linear
     public static int factorialR(int num){
-        if (num == 0) 
-            return 1;
+        if (num == 0)                       // 1 * (N+1) = N + 1
+            return 1;                       // 1
         else
-            return num*factorialR(num-1);
+            return num*factorialR(num-1);   // (1 + 1 + 1 + 1) * N = 4N
     }
     
 }
