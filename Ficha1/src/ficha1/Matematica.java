5b372b7841072c5b9614e01ed3c5b32b38ba15d2
Paulo Santos <pauloagsantos@gmail.com>
2025-09-26 09:53:40 +0100

    adaptacao para BigInteger

--- Ficha1/src/ficha1/Matematica.java
+++ Ficha1/src/ficha1/Matematica.java
@@ -4,32 +4,37 @@
  */
 package ficha1;
 
+import java.math.BigInteger;
+
 /**
  *
  * @author IPT
  */
 public class Matematica {
     
+    // estudo feito antes de adaptar para o BigInteger
     // 5N+4
     // O(N) Complexidade temporal linear
     // num+f+i+return (4 variáveis)  
     // S(1) Complexidade espacial constante
-    public static int factorialI(int num){
-        int f = 1;                     // 1
-        for(int i = 1; i <= num; i++)  // 1 + N + 1 + 2N
-            f*=i;                      // 2N
-        return f;                      // 1
+    public static BigInteger factorialI(int num){
+        BigInteger f = BigInteger.ONE;            // 1
+        for(int i = 1; i <= num; i++)             // 1 + N + 1 + 2N
+            f=f.multiply(BigInteger.valueOf(i));  // 2N
+        return f;                                 // 1
     }
     
+    // estudo feito antes de adaptar para o BigInteger
     // 5N + 2
     // O(N) Complexidade temporal linear
     // (num+return) * (N+1 da Recursividade) = 2N+2
     // S(N) Complexidade espacial linear
-    public static int factorialR(int num){
+    public static BigInteger factorialR(int num){
         if (num == 0)                       // 1 * (N+1) = N + 1
-            return 1;                       // 1
+            return BigInteger.ONE;          // 1
         else
-            return num*factorialR(num-1);   // (1 + 1 + 1 + 1) * N = 4N
+            // (1 + 1 + 1 + 1) * N = 4N
+            return BigInteger.valueOf(num).multiply(factorialR(num-1));   
     }
     
 }
